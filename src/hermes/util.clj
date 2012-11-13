cd073e84a5ba66c08ea711afa627ef7bc4426d79
Zack Maril <zack@gameclosure.com>
2012-11-13 12:32:48 -0800

    Moved hermes.reflect into the utils namespace.

--- src/hermes/util.clj
+++ src/hermes/util.clj
@@ -1,4 +1,6 @@
-(ns hermes.util)
+(ns hermes.util
+  (:require [clojure.reflect :as r])
+  (:use [clojure.pprint :only (pprint)]))
 
 ;;https://groups.google.com/forum/?fromgroups=#!topic/clojure/GAGF38uI1-o
 (defn- merge-meta! 
@@ -18,4 +20,13 @@ namespace."
       (merge-meta! v 
         (if (.isBound v) 
           (intern *ns* sym (var-get v)) 
-          (intern *ns* sym)))))) 
+          (intern *ns* sym))))))
+
+(defn members-of-object [object]
+  (-> object r/reflect :members))
+
+(defmacro find-member [object prop]
+  `(filter #(= '~prop (:name %)) (members-of-object ~object)))
+
+(defn names-of-members [object]
+  (sort (distinct (map :name (members-of-object object)))))
