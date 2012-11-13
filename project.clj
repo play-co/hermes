197d7daf33687e46087d145340b0711dea157080
Zack Maril <zack@gameclosure.com>
2012-11-13 15:19:54 -0800

    Bumping to 0.2.0 for the next push to clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hermes "0.1.9"
+(defproject hermes "0.2.0"
   :description "Embedded Titan Graph"
   :url "https://github.com/gameclosure/hermes"
   :license {:name "Eclipse Public License"
