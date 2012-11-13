d7b5baf6d09956a3059e5ac6824f3672eb3089e9
Zack Maril <zack@gameclosure.com>
2012-11-12 21:51:21 -0800

    Namespaced functions to get rid of the immigration function call in test

--- test/hermes/edge_test.clj
+++ test/hermes/edge_test.clj
@@ -5,14 +5,12 @@
             [hermes.edge :as e]
             [hermes.vertex :as v]))
 
-(immigrate 'hermes.element)
-
 (deftest test-endpoints
   (g/open)
   (let [v1 (v/create {:name "v1"})
         v2 (v/create {:name "v2"})
         edge (first (e/upconnect! v1 v2 "connexion"))]
-    (is (= ["v1" "v2"] (map #(get-property % :name) (e/endpoints edge))))))
+    (is (= ["v1" "v2"] (map #(e/get-property % :name) (e/endpoints edge))))))
 
 (deftest test-upconnect!
   (testing "Upconnecting once"
@@ -23,7 +21,7 @@
       (is (e/connected? v1 v2))
       (is (e/connected? v1 v2 "connexion"))
       (is (not (e/connected? v2 v1)))
-      (is (= "the edge" (get-property edge :name)))
+      (is (= "the edge" (e/get-property edge :name)))
       (is (= 1 (count (seq (.getEdges g/*graph*)))))))
 
   (testing "Upconnecting multiple times"
@@ -36,7 +34,7 @@
       (is (e/connected? v1 v2))
       (is (e/connected? v1 v2 "connexion"))
       (is (not (e/connected? v2 v1)))
-      (is (= "the edge" (get-property edge :name)))
+      (is (= "the edge" (e/get-property edge :name)))
       (is (= 1 (count (seq (.getEdges g/*graph*))))))))
 
 
