df483f616eb5da3e54693564f3960a1d0caa6e5a
Zack Maril <zack@gameclosure.com>
2012-11-20 15:16:33 -0800

    Bump project number

--- src/hermes/example.clj
+++ src/hermes/example.clj
@@ -4,36 +4,35 @@
             [hermes.edge   :as e]
             [hermes.type   :as t]))
 
-(g/open {:storage {:backend "cassandra"
-                   :hostname "127.0.0.1"}})
+(g/open)
 
 
-(g/transact! (t/create-vertex-key-once :name String {:unique true
+(g/transact! (t/create-vertex-key-once :name-a String {:unique true
                                                      :indexed true}))
 
-(def Zack (g/transact! (first (v/upsert! :name
-                                         {:name "Zack"
+(def Zack (g/transact! (first (v/upsert! :name-a
+                                         {:name-a "Zack"
                                           :age 21
                                           :gender "Male"
                                           :occupation "INTERN"}))))
 
-(def Brooke (g/transact! (first (v/upsert! :name
-                                           {:name "Brooke"
+(def Brooke (g/transact! (first (v/upsert! :name-a
+                                           {:name-a "Brooke"
                                             :age 19
                                             :gender "Female"
                                             :occupation "Student"}))))
 
-(g/transact! (e/connect! (v/refresh Zack)
-                         (v/refresh Brooke)
-                         "siblings" {:since 1991}))
+(g/transact! (println "connect" (e/connect! (v/refresh Zack)
+                                  (v/refresh Brooke)
+                                  "siblings" {:since 1991})))
 
 ;;Oops mistake, should probably fix that
-(g/transact! (e/upconnect! (first (v/find-by-kv :name "Zack"))
-                           (v/refresh Brooke)
-                           "siblings" {:since 1993}))
+(g/transact! (println "upconnect!" (e/upconnect! (first (v/find-by-kv :name-a "Zack"))
+                                    (v/refresh Brooke)
+                                    "siblings" {:since 1993})))
 
 ;;And well, I am really more than just an intern
-(g/transact! (v/upsert! :name {:name "Zack"
+(g/transact! (v/upsert! :name-a {:name-a "Zack"
                                :occupation "Software specialist and all around nice guy"}))
 
 (defn -main [& args]
